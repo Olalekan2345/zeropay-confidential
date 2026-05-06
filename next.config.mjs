db08855db2b853ff1532a07af53dff963978cc9a
olabanjiolalekan12345 <olabanjiolalekan12345@gmail.com>
2026-05-06 11:59:48 +0100

    fix: alias react-native async-storage for web build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['api.dicebear.com'],
   },
+  webpack: (config) => {
+    config.resolve.alias['@react-native-async-storage/async-storage'] = false;
+    return config;
+  },
 };
 
 export default nextConfig;
